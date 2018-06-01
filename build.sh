8a3be80c7418089d1e9e1b532a3f1cd7c001fd18
Alexander Artemenko <svetlyak.40wt@gmail.com>
2018-06-01 09:28:26 +0300

    Build.sh now uses ceramic.ros to build an application.

--- build.sh
+++ build.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-
-# ros  -l "/Users/art/projects/lisp/hacrm/quicklisp/setup.lisp" \
-#      -e "(setf *debugger-hook* #'(lambda (c h) (declare (ignore h)) (uiop:print-condition-backtrace c) (uiop:quit -1)))" \
-#      -e "(asdf:load-system :HACRM)" \
-#      -e "(setf uiop:*image-entry-point* #'(lambda () (ceramic-entry::hacrm :releasep nil)))" \
-#      -e "(uiop:dump-image #P\"/Users/art/.ceramic/working/hacrm\" :executable t
-#                #+sb-core-compression :compression #+sb-core-compression t)"
-
-
-CL_SOURCE_REGISTRY=~/common-lisp// qlot exec ros -L sbcl-bin build app.ros
+CL_SOURCE_REGISTRY=~/common-lisp// qlot exec ./ceramic.ros --verbose app.ros
